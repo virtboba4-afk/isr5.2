ddf290114ab13f67614b0dd381bad496b6238726
Nikita <easypizi92@mail.ru>
2025-11-24 13:28:52 +0300

    обавлена переменная x (коммит 2)

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ class Main {
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
         System.out.println("Новое сообщение для коммита 1");
+
+        int x = 10;
+        System.out.println("Значение x = " + x);
     }
 }
